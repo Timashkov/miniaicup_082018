c473ba1a2207a11d63b9b1c41dcbf76113570042
Aleksei Timashkov <osb.zla@gmail.com>
2018-09-04 23:48:00 +0300

    conversion

---
+++ v1_cpp/game/Buggy.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Buggy.h
+ * Author: alex
+ *
+ * Created on September 4, 2018, 10:37 PM
+ */
+
+#ifndef BUGGY_H
+#define BUGGY_H
+
+class Buggy {
+public:
+    Buggy();
+    Buggy(const Buggy& orig);
+    virtual ~Buggy();
+private:
+
+};
+
+#endif /* BUGGY_H */
+
