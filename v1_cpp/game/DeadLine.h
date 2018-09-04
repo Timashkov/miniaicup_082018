06f65d992365dc475f32617b0ad9ec4e1e84763c
Aleksei Timashkov <osb.zla@gmail.com>
2018-09-04 23:48:05 +0300

    conversion

---
+++ v1_cpp/game/DeadLine.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   DeadLine.h
+ * Author: alex
+ *
+ * Created on September 4, 2018, 10:39 PM
+ */
+
+#ifndef DEADLINE_H
+#define DEADLINE_H
+
+class DeadLine {
+public:
+    DeadLine();
+    DeadLine(const DeadLine& orig);
+    virtual ~DeadLine();
+private:
+
+};
+
+#endif /* DEADLINE_H */
+
